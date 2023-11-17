8bfa8acd6420bb41d2b30c9f4c749151a20861bb
“camillebrown” <camillebrown11@gmail.com>
2023-11-17 12:08:56 -0800

    Update construction mobile view

--- src/components/pages/UnderConstruction.js
+++ src/components/pages/UnderConstruction.js
@@ -12,19 +12,19 @@ export default function UnderConstruction() {
   return (
     <div className="bg-gradient-to-bl from-normal-tan from-2% via-mustard via-20% to-sage to-90% w-screen h-screen flex flex-col items-center justify-center">
       <RevealOnScroll parentclassName="w-3/5 mx-auto flex flex-col items-center justify-center">
-        <p className="font-dream-ave text-7xl text-white text-center">
+        <p className="font-dream-ave text-mobile-fluid sm:text-7xl text-white text-center">
           Under Construction
         </p>
-        <div className="w-full mt-6">
+        <div className="w-[90%] mx-auto mt-6 text-center">
           <div className="w-full flex flex-col justify-center items-center">
-            <p className="font-montserrat font-extralight text-lg text-white tracking-ultra-wide uppercase">
+            <p className="font-montserrat font-extralight fluid sm:text-lg text-white tracking-ultra-wide uppercase">
               Site Nearly Ready
             </p>
-            <p className="font-montserrat font-extralight text-sm text-white tracking-wide">
+            <p className="w-4/5 xs:w-full font-montserrat font-extralight text-xs sm:text-sm text-white tracking-wide">
               Everything needs a little revamping, right? It'll be right back up
               soon!
             </p>
-            <div className="p-4 border border-white rounded-md my-6 w-4/5">
+            <div className="py-2 px-4 sm:p-4 border border-white rounded-md my-6 w-4/5">
               <div className="flex items-center gap-x-3 whitespace-nowrap">
                 <div
                   className="flex w-full h-2 bg-deep-gray bg-opacity-20 rounded-full overflow-hidden"
@@ -43,24 +43,24 @@ export default function UnderConstruction() {
               </div>
             </div>
           </div>
-          <div className="flex items-center justify-center gap-4">
+          <div className="flex flex-col xs:flex-row items-center justify-center gap-2 xs:gap-4">
             <p
               onClick={() => navigate(`/work/${projectID}`)}
-              className="text-center font-montserrat font-light text-sm text-white tracking-ultra-wide whitespace-nowrap hover:cursor-pointer rounded-md bg-deep-gray bg-opacity-20 py-2 px-4 hover:bg-opacity-40"
+              className="text-center font-montserrat font-light text-xs xs:text-sm text-white tracking-ultra-wide whitespace-nowrap hover:cursor-pointer rounded-md bg-deep-gray bg-opacity-20 py-2 px-4 hover:bg-opacity-40"
             >
               Back to {projectTitle}
             </p>
             <p
               onClick={() => navigate(`/work`)}
-              className="text-center font-montserrat font-light text-sm text-white tracking-ultra-wide whitespace-nowrap hover:cursor-pointer rounded-md bg-deep-gray bg-opacity-20 py-2 px-4 hover:bg-opacity-40"
+              className="text-center font-montserrat font-light text-xs xs:text-sm text-white tracking-ultra-wide whitespace-nowrap hover:cursor-pointer rounded-md bg-deep-gray bg-opacity-20 py-2 px-4 hover:bg-opacity-40"
             >
               Back to Projects
             </p>
           </div>
         </div>
       </RevealOnScroll>
-      <div className="w-3/5 mx-auto flex items-end justify-center">
-        <div className="flex items-start justify-around mt-32 w-1/2">
+      <div className="w-full xs:w-3/5 mx-auto flex items-end justify-center">
+        <div className="flex items-start justify-center gap-2 xs:gap-0 xs:justify-around mt-6 xs:mt-32 w-full xs:w-1/2">
           <Link
             to="https://www.instagram.com/camilllebrown/"
             className="rounded-full border border-white p-3 hover:bg-deep-gray hover:bg-opacity-20 hover:cursor-pointer"
